09ec24f1a19ffef4b675053284c4467e70c9dd5c
rhiokim <rhio.kim@gmail.com>
2018-03-17 11:34:41 +0900

    feat(cmp): Panel update styles

--- services/components/src/Panel.js
+++ services/components/src/Panel.js
@@ -1,7 +1,13 @@
 // @flow
 import React from 'react';
 
-export default ({ children }) => <div>
+const style = {
+  backgroundColor: 'rgba(255,255,255,1)',
+  boxShadow: 'rgba(0, 0, 0, 0.12) 0px 1px 6px, rgba(0, 0, 0, 0.12) 0px 1px 4px',
+  padding: '5px'
+}
+
+export default ({ children }) => <div style={style}>
   <h2>Panel header</h2>
   {children}
 </div>
