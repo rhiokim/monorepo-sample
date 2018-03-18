673c2afaeefd28400cdd09713c0c997d53851cc0
rhiokim <rhio.kim@gmail.com>
2018-03-18 14:12:02 +0900

    fix: eslint rules for components module

---
+++ services/components/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  parser: 'babel-eslint',
+  env: {
+    es6: true,
+    jest: true
+  },
+  globals: {
+    page: true,
+    browser: true,
+    expectPage: true
+  },
+  plugins: ['flowtype', 'react'],
+  extends: ['standard', 'standard-flow', 'react-app'],
+  settings: {
+    flowtype: {
+      onlyFilesWithFlowAnnotation: false
+    }
+  },
+  rules: {
+    'jsx-a11y/href-no-hash': 'off',
+    'import/first': 'off',
+    'import/no-webpack-loader-syntax': 'off'
+  }
+}
